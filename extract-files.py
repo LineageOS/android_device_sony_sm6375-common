33ead21debb5471121f2d7339a6bd950e2952c5a
LuK1337 <priv.luk@gmail.com>
2025-03-31 22:10:54 +0200

    sm6375-common: Allow extra syscalls for wfdservice
    Required for 15 QPR2.
    
    Change-Id: Ifcc736e2aa2bff313825a95ab2df68a9b053cd5e

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,9 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/seccomp_policy/wfdservice.policy': blob_fixup()
+        .add_line_if_missing('memfd_create: 1')
+        .add_line_if_missing('rt_tgsigqueueinfo: 1'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/bin/thermal-engine': blob_fixup()
