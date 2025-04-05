14aacd6bfcba377f62a4d1b3cc56b85e0ba1f52e
LuK1337 <priv.luk@gmail.com>
2025-04-05 22:18:31 +0200

    sm6375-common: libwfdservice_shim_v1 -> libwfdservice_shim
    Change-Id: Idf65dd61a2de80dfcbf72a8b75b6ca59234df573

--- extract-files.py
+++ extract-files.py
@@ -47,7 +47,7 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice': blob_fixup()
-        .add_needed('libwfdservice_shim_v1.so'),
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/bin/thermal-engine': blob_fixup()
