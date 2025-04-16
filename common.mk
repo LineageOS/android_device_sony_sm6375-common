4758d70626cf403ff9ae7f77fbe9c4b4f25feb96
LuK1337 <priv.luk@gmail.com>
2025-04-16 14:04:21 +0200

    sm6375-common: Set correct first api level
    $ rg ro.board.first_api_level XQ-*/raw/vendor/build.prop
    XQ-CC54_Customized_EU-UK_65.2.A.2.224/raw/vendor/build.prop
    41:ro.board.first_api_level=30
    
    XQ-DC54_Customized_EU-UK_68.2.A.2.67/raw/vendor/build.prop
    41:ro.board.first_api_level=30
    
    Change-Id: I30260994303be97136a7e99ee7b16e5c4ff45e1e

--- common.mk
+++ common.mk
@@ -283,7 +283,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_holi/android.hardware.sensor.stepdetector.xml
 
 # Shipping API
-BOARD_SHIPPING_API_LEVEL := 33
+BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Sony device library
