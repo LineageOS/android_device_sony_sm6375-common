4b1ec0bc6d4001f456f1e5f311c546e090318c83
LuK1337 <priv.luk@gmail.com>
2026-03-10 09:12:28 +0100

    sm6375-common: Remove fwk-detect lib from PRODUCT_PACKAGES
    See: Iac75d83e6bc46c137c328a2365e6701c210bfe7a
    Change-Id: I6b8571e81e59f438e34b76823f47ff024ec33dc6

--- common.mk
+++ common.mk
@@ -267,10 +267,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/holi/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# QTI fwk-detect
-PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
